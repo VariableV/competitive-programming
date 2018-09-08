e7b3059a9ac3b19570bbbf3c2f6e435e119f5f5e
VariableV <student.mhuss@gmail.com>
2018-09-08 13:25:15 -0500

    beads finished

--- USACO/beads/beads.java
+++ USACO/beads/beads.java
@@ -5,30 +5,39 @@ TASK: beads
 */
 import java.io.*;
 public class beads{
-	static String str;
-	static int bruteforce(int idx, String currentStr){
-		if (idx == str.length()) return currentStr.length();
-		char currentChar = str.charAt(idx);
-		if (currentChar == 'w')
-			return Math.max(bruteforce(idx+1, currentStr + "b"), bruteforce(idx+1, currentStr + "r"));
-		if (currentStr.length() == 0)
-			return bruteforce(idx+1, currentStr + currentChar);
-		if (currentChar == currentStr.charAt(currentStr.length()-1))
-			return bruteforce(idx+1, currentStr + currentChar);
-		else{
-		
-		System.out.println(currentStr);
-			// reached a wrong character, reset the counter to 0, start over currentStr
-			return bruteforce(idx+1, "" + currentChar);
-		}
+	static char[] str;
+	static int slen;
+	static int mod(int n, int m){
+		while (n<0)
+			n+=m;
+		return n%m;
+	}
+	static int beadcount(int pos, boolean dir){
+		int n=0;
+		char color = 'w';
+		if (!dir) pos--;
+		for(int i = 0; i<slen; i++){
+				char c=str[mod(pos+(dir ? i : -i),slen)];
+				if (color == 'w' && c != 'w')
+					color = c;
+
+				if (color != 'w' && c != 'w' && c!=color)break;
+				n++;
+			}
+		return n;
 	}
 	public static void main(String[] args)throws IOException{
 		BufferedReader key=new BufferedReader(new FileReader("beads.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("beads.out")));
 		int len = Integer.parseInt(key.readLine());
-		str = key.readLine();
-		//str += str;
-		//System.out.println(str);
-		System.out.println(bruteforce(0, ""));
+		str = key.readLine().toCharArray();
+		slen = str.length;
+		int max=0;
+		for(int i = 0; i < slen; i++){
+			max = Math.max(beadcount(i, true)+beadcount(i, false),max);
+		}
+		max=Math.min(max,slen);
+		out.println(max);
+		out.close();
 	}
 }
